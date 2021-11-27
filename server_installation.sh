65ec1cedfc86dc7ed3d0688664baa291fb63f760
Kashish Dhawan <56489521+Kashishdwn@users.noreply.github.com>
2021-11-27 13:17:23 +0530

    Create server_installation.sh

---
+++ server_installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo amazon-linux-extras install epel -y
+sudo yum install jenkins java-1.8.0-openjdk-devel -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install packer
